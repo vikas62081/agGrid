3aab67a7069f0d5cd7cc8682c00bd8d0760f596a
vikas6208 <vikas62081@gmail.com>
2021-07-27 16:34:23 +0530

    pushing crud operation in ag grid with server side

--- src/App.js
+++ src/App.js
@@ -1,93 +1,121 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
+import './App.css';
 import { AgGridReact } from 'ag-grid-react';
-import 'ag-grid-enterprise';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
-import _ from 'lodash'
-const App = () => {
-  const [gridApi, setGridApi] = useState(null);
-  const [searchText, setSearchText] = useState('')
-  const columns = [
-    { headerName: "Athlete", field: "athlete", filter: "agTextColumnFilter" },
-    { headerName: "Age", field: "age", filter: "agTextColumnFilter" },
-    { headerName: "Country", field: "country", filter: "agTextColumnFilter" },
-    { headerName: "Year", field: "year", filter: "agTextColumnFilter" },
-    { headerName: "Date", field: 'date', filter: "agTextColumnFilter" },
-    { headerName: "Sport", field: 'sport', filter: "agTextColumnFilter" },
-    { headerName: "Gold", field: 'gold', filter: "agTextColumnFilter" },
-    { headerName: "Silver", field: 'silver', filter: "agTextColumnFilter" },
-    { headerName: "Bronze", field: 'bronze', filter: "agTextColumnFilter" },
-    { headerName: "Total", field: 'total', filter: "agTextColumnFilter" },
-  ]
-
-  useEffect(() => {
-    // console.log(searchText)
-    if (gridApi) {
-      onGridReady(gridApi)
-    }
-  }, [searchText])
-
-  const search = _.debounce((text) => {
-    // console.log(text)
-    setSearchText(text)
-  }, 1000)
-  const datasource = {
-    getRows(params) {
-      console.log(JSON.stringify(params.request, null, 1));
-      const { startRow, endRow, filterModel, sortModel } = params.request
-      let url = `http://localhost:4000/olympic?`
-      //Quick filter
-      if (searchText) {
-        url += `q=${searchText}&`
-      }
-      //Sorting
-      if (sortModel.length) {
-        const { colId, sort } = sortModel[0]
-        url += `_sort=${colId}&_order=${sort}&`
-      }
-      //Filtering
-      const filterKeys = Object.keys(filterModel)
-      filterKeys.forEach(filter => {
-        url += `${filter}=${filterModel[filter].filter}&`
-      })
-      //Pagination
-      url += `_start=${startRow}&_end=${endRow}`
-      fetch(url)
-        .then(httpResponse => httpResponse.json())
-        .then(response => {
-          params.successCallback(response, 499);
-        })
-        .catch(error => {
-          console.error(error);
-          params.failCallback();
-        })
-    }
+import { Grid, Button } from '@material-ui/core'
+import FormDialog from './components/dialog';
+const initialValue = { name: "", email: "", phone: "", dob: "" }
+function App() {
+  const [gridApi, setGridApi] = useState(null)
+  const [tableData, setTableData] = useState(null)
+  const [open, setOpen] = React.useState(false);
+  const [formData, setFormData] = useState(initialValue)
+  const handleClickOpen = () => {
+    setOpen(true);
   };
 
+  const handleClose = () => {
+    setOpen(false);
+    setFormData(initialValue)
+  };
+  const url = `http://localhost:4000/users`
+  const columnDefs = [
+    { headerName: "ID", field: "id" },
+    { headerName: "Name", field: "name", },
+    { headerName: "Email", field: "email", },
+    { headerName: "phone", field: "phone" },
+    { headerName: "Date of Birth", field: "dob" },
+    {
+      headerName: "Actions", field: "id", cellRendererFramework: (params) => <div>
+        <Button variant="outlined" color="primary" onClick={() => handleUpdate(params.data)}>Update</Button>
+        <Button variant="outlined" color="secondary" onClick={() => handleDelete(params.value)}>Delete</Button>
+      </div>
+    }
+  ]
+  // calling getUsers function for first time 
+  useEffect(() => {
+    getUsers()
+  }, [])
+
+  //fetching user data from server
+  const getUsers = () => {
+    fetch(url).then(resp => resp.json()).then(resp => setTableData(resp))
+  }
+  const onChange = (e) => {
+    const { value, id } = e.target
+    // console.log(value,id)
+    setFormData({ ...formData, [id]: value })
+  }
   const onGridReady = (params) => {
-    setGridApi(params);
-    // register datasource with the grid
-    params.api.setServerSideDatasource(datasource);
+    setGridApi(params)
   }
 
+  // setting update row data to form data and opening pop up window
+  const handleUpdate = (oldData) => {
+    setFormData(oldData)
+    handleClickOpen()
+  }
+  //deleting a user
+  const handleDelete = (id) => {
+    const confirm = window.confirm("Are you sure, you want to delete this row", id)
+    if (confirm) {
+      fetch(url + `/${id}`, { method: "DELETE" }).then(resp => resp.json()).then(resp => getUsers())
+
+    }
+  }
+  const handleFormSubmit = () => {
+    if (formData.id) {
+      //updating a user 
+      const confirm = window.confirm("Are you sure, you want to update this row ?")
+      confirm && fetch(url + `/${formData.id}`, {
+        method: "PUT", body: JSON.stringify(formData), headers: {
+          'content-type': "application/json"
+        }
+      }).then(resp => resp.json())
+        .then(resp => {
+          handleClose()
+          getUsers()
+
+        })
+    } else {
+      // adding new user
+      fetch(url, {
+        method: "POST", body: JSON.stringify(formData), headers: {
+          'content-type': "application/json"
+        }
+      }).then(resp => resp.json())
+        .then(resp => {
+          handleClose()
+          getUsers()
+        })
+    }
+  }
+
+  const defaultColDef = {
+    sortable: true,
+    flex: 1, filter: true,
+    floatingFilter: true
+  }
   return (
-    <div>
+    <div className="App">
       <h1 align="center">React-App</h1>
-      <h4 align='center'>Implement Server-Side Quick Filter in ag Grid</h4>
-      <input type="search" placeholder="Search something..." onChange={e => search(e.target.value)}
-        style={{ padding: 10, fontSize: "105%", width: "100%", outline: 0 }} />
-      <div className="ag-theme-alpine">
+      <h3>CRUD Operation with Json-server in ag-Grid</h3>
+      <Grid align="right">
+        <Button variant="contained" color="primary" onClick={handleClickOpen}>Add user</Button>
+      </Grid>
+      <div className="ag-theme-alpine" style={{ height: '400px' }}>
         <AgGridReact
-          columnDefs={columns}
-          pagination={true}
-          paginationPageSize={8}
-          domLayout="autoHeight"
-          rowModelType="serverSide"
+          rowData={tableData}
+          columnDefs={columnDefs}
+          defaultColDef={defaultColDef}
           onGridReady={onGridReady}
-          defaultColDef={{ filter: true, floatingFilter: true, sortable: true }}
         />
       </div>
+      <FormDialog open={open} handleClose={handleClose}
+        data={formData} onChange={onChange} handleFormSubmit={handleFormSubmit} />
     </div>
   );
-};
-export default App
+}
+
+export default App;
---
+++ src/components/dialog.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import { TextField } from '@material-ui/core';
+
+export default function FormDialog({open,handleClose,data,onChange,handleFormSubmit}) {
+ const {id,name,email,phone,dob}=data
+
+  return (
+    <div>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{id?"Update user":"Create new user"}</DialogTitle>
+        <DialogContent>
+         <form>
+             <TextField id="name" value={name} onChange={e=>onChange(e)} placeholder="Enter name" label="Name" variant="outlined" margin="dense" fullWidth />
+             <TextField id="email" value={email} onChange={e=>onChange(e)} placeholder="Enter email" label="Email" variant="outlined" margin="dense" fullWidth />
+             <TextField id="phone" value={phone} onChange={e=>onChange(e)} placeholder="Enter phone number" label="Phone Number" variant="outlined" margin="dense" fullWidth />
+             <TextField id="dob" value={dob} onChange={e=>onChange(e)} placeholder="Enter Date of birth" label="Date of Birth" variant="outlined" margin="dense" fullWidth />
+         </form>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="secondary" variant="outlined">
+            Cancel
+          </Button>
+          <Button  color="primary" onClick={()=>handleFormSubmit()} variant="contained">
+            {id?"Update":"Submit"}
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
